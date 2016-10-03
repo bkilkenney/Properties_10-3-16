5555b3a82ad206ce593e0c6327752bedf6d2c761
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-03 16:45:54 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_10_3_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TimePeriod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_10_3_16
+{
+    class TimePeriod
+    {
+
+        private double seconds;
+
+        public double Hours
+        {
+            get { return seconds / 3600; }
+            set { seconds = value * 3600;  } 
+        }
+
+
+    }
+}
